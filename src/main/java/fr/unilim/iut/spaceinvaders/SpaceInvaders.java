5841956f7cbc15404e246ece0fde2bbd3276eaaf
Léo Mazabraud <leo.mazabraud@iut.unilim.fr>
2020-05-11 12:27:23 +0200

    Story n2 + debut refactoring (arrêt au 3)

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -4,6 +4,7 @@ package fr.unilim.iut.spaceinvaders;
 
 	    int longueur;
 	    int hauteur;
+	    Vaisseau vaisseau;
 
 	    public SpaceInvaders(int longueur, int hauteur) {
 		   this.longueur = longueur;
@@ -13,12 +14,37 @@ package fr.unilim.iut.spaceinvaders;
 	    @Override
 		public String toString() {
 			StringBuilder espaceDeJeu = new StringBuilder();
-			for (int i = 0; i < hauteur; i++) {
-				for (int j = 0; j < longueur; j++) {
-					espaceDeJeu.append('.');
+			for (int y = 0; y < hauteur; y++) {
+				for (int x = 0; x < longueur; x++) {
+					espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 				}
 				espaceDeJeu.append('\n');
 			}
 			return espaceDeJeu.toString();
 		}
+
+		private char recupererMarqueDeLaPosition(int x, int y) {
+			char marque;
+			if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+				marque='V';
+			else
+				marque='.';
+			return marque;
+		}
+
+		private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+			return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+		}
+
+		private boolean aUnVaisseau() {
+			return vaisseau!=null;
+		}
+
+		public void positionnerUnNouveauVaisseau(int i, int j) {
+			this.vaisseau = new Vaisseau (i,j);
+			
+		}
+
+		//refactoring 3 Ameliorer la quali en éliminant les nbs magiques
+		
    }
